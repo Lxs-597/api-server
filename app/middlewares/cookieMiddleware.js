1a5d0db980fc87b60979fe62e140a528bce657a0
Lxs-597 <312181653@qq.com>
2019-01-03 16:36:20 +0800

    删除cookie信息

--- app/middlewares/cookieMiddleware.js
+++ app/middlewares/cookieMiddleware.js
@@ -1,6 +1,5 @@
 module.exports = async (ctx, next) => {
-  ctx.cookies.set('JSESSIONID', '0A19B3DD3A705A5E4295E91B0B27D363')
-  ctx.request.header.Cookie = 'JSESSIONID=0A19B3DD3A705A5E4295E91B0B27D363'
+  ctx.cookies.set('', '')
 
   await next()
 }
